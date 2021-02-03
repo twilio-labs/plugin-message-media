0703733207c2e139acc9c78d0d3b436091f23d9b
Vinicius Miguel <vini.miguel1994@gmail.com>
2021-02-03 17:30:40 -0300

    Update Flex plugin version

---
+++ plugin-message-media/public/appConfig.example.js
@@ -0,0 +1,14 @@
+var accountSid = ''
+
+var appConfig = {
+  pluginService: {
+    enabled: true,
+    url: '/plugins',
+  },
+  sso: {
+    accountSid: accountSid
+  },
+  ytica: false,
+  logLevel: 'info',
+  showSupervisorDesktopView: true,
+};
